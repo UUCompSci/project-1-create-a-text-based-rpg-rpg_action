68a6a838269febb65742845f5ff9844f78999b04
fakemccoy1800 <>
2025-09-20 12:58:09 -0500

    check and see if it is updated.

---
+++ RPG_action/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Which number do you want?");
+
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] regions = [1, 2, 3, 4, 5, 6, 7];
+
+
+
+string[] dialogue_tree = [
+    "yes",
+    "no",
+    "start the game already",
+    "long time no siege"];
+
+
+
+switch (n)
+{
+    case 1:
+        Console.WriteLine(dialogue_tree[0]);
+        break;
+    case 2:
+        Console.WriteLine(dialogue_tree[1]);
+        break;
+    case 3:
+        Console.WriteLine(dialogue_tree[2]);
+        break;
+    case 4:
+        Console.WriteLine(dialogue_tree[3]);
+        break;
+    default:
+
+        break;
+
+}
